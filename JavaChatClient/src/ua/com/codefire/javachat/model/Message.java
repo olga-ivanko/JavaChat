aa3dc0e73479b091f693e139723e9ee319b8f315
olga.ivanko <o.ivanko@me.com>
2016-07-13 12:01:22 +0300

    New class Message was added.

---
+++ JavaChatClient/src/ua/com/codefire/javachat/model/Message.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.com.codefire.javachat.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author homefulloflove
+ */
+public class Message {
+
+    private Date timestamp;
+    private boolean income;
+    private String text;
+    private boolean read;
+
+    /**
+     * Constructor for outgoing message creation.
+     *
+     * @param timestamp time when message was sent.
+     * @param text message content.
+     */
+    public Message(Date timestamp, String text) {
+        this.timestamp = timestamp;
+        this.text = text;
+        this.read = true;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public boolean isIncome() {
+        return income;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    /**
+     * Status of the message set as read.
+     */
+    public void read() {
+        this.read = true;
+    }
+
+    
+}
